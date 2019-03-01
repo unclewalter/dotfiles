b058c850cbf696c7d153c3df328a4bc6c8e05320
Kevan Atkins <kevan@kevanatkins.com>
2019-03-02 02:37:02 +1100

    Configuring emacs to autosave in the emacs directory rather than in-place

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,8 @@
+					;Moving auto saves to emacs directory
+(setq auto-save-file-name-transforms
+          `((".*" ,(concat user-emacs-directory "auto-save/") t))) 
+					;Enable Evil mode
+
 (add-to-list 'load-path "~/.emacs.d/evil")
 (require 'evil)
 (evil-mode 1)
