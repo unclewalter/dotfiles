cf4e1aa68c0520b1dd19c48f160b8c3f598ebfda
Kevan Atkins <kevan@kevanatkins.com>
2019-03-02 00:16:05 +1100

    Updating bootstrap script and fixing spelling error

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,3 +8,13 @@ else
     echo "Updating Homebrew"
     brew update
 fi
+
+if [[ $(command -v brew) == "" ]]; then
+    echo "Installing zsh"
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+else
+    echo "Updating Homebrew"
+    brew update
+fi
+
+sh update_dotfiles.sh
