d2a90a53ca890ea152fbd24acbf29cac6b543b5c
Kevan Atkins <kevan@kevanatkins.com>
2019-03-02 03:09:52 +1100

    Adding org mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -6,3 +6,11 @@
 (add-to-list 'load-path "~/.emacs.d/evil")
 (require 'evil)
 (evil-mode 1)
+
+					; Enable org mode
+(add-to-list 'load-path "~/.emacs.d/plugins/evil-org")
+(require 'evil-org)
+(add-hook 'org-mode-hook 'evil-org-mode)
+(evil-org-set-key-theme '(navigation insert textobjects additional calendar))
+(require 'evil-org-agenda)
+(evil-org-agenda-set-keys)
