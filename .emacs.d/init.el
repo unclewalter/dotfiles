e938c167843b63b23da3d2ea6be9124a1f8a4a20
Kevan Atkins <kevan@kevanatkins.com>
2019-03-05 02:18:20 +1100

    Adding dracula theme

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -12,3 +12,6 @@
 (require 'evil-org)
 (add-hook 'org-mode-hook 'evil-org-mode)
 (evil-org-set-key-theme '(navigation insert textobjects additional calendar))
+
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
+(load-theme 'dracula t)
